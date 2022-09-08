4c3497ad7907e8fa4c934905d9b9edf3433fabb8
Chenglin Wang <cwangci@connect.ust.hk>
2022-09-08 17:04:25 +0800

    sync

---
+++ test.c
@@ -0,0 +1,21 @@
+#define LEN 32000
+#define LEN2 256
+
+#define ntimes 50000
+ float a[LEN],b[LEN],c[LEN],d[LEN],e[LEN],
+                                   aa[LEN2][LEN2],bb[LEN2][LEN2],cc[LEN2][LEN2],tt[LEN2][LEN2];
+int s276(int q)
+{
+	int mid = (LEN/2);
+	for (int nl = 0; nl < 4*ntimes; nl++) {
+		for (int i = 0; i < LEN; i++) {
+			i = i + 1;
+			if (i+1 < mid) {
+				a[i] += b[i] * c[i];
+			} else {
+				a[i] += b[i] * d[i];
+			}
+		}
+	}
+	return 0;
+}
